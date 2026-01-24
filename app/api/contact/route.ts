4efc7ae7d6b113dbff90fa46c432f0cc4c4c1cdb
Fine-Jason <>
2026-01-24 16:52:36 -0500

    removed analytics

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,17 +2,16 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const formLink = process.env.GOOGLE_FORM_LINK;
-  if (!formLink) {
-    return new NextResponse("Please configure the env variables", {
-      status: 500,
-    });
-  }
 
   // configure this according to your google form
   const fieldIdName = process.env.GOOGLE_FORM_FIELD_ID_NAME;
   const fieldIdEmail = process.env.GOOGLE_FORM_FIELD_ID_EMAIL;
   const fieldIdMessage = process.env.GOOGLE_FORM_FIELD_ID_MESSAGE;
   const fieldIdSocial = process.env.GOOGLE_FORM_FIELD_ID_SOCIAL;
+
+  if (!formLink || !fieldIdName || !fieldIdEmail || !fieldIdMessage || !fieldIdSocial) {
+    return NextResponse.json({message: "Contact form not configured"}, { status: 200 });
+  }
 
   try {
     const body = await req.json();
