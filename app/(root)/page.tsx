58fab67b519d24eb3486f46ea9cb48478b494426
Fine-Jason <>
2026-01-25 15:10:55 -0500

    edit 1 on the experience page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -126,7 +126,7 @@ export default function IndexPage() {
                     size: "lg",
                   })
                 )}
-                aria-label="Contact Naman Barkiya"
+                aria-label="Contact Jason Fine"
               >
                 <Icons.contact className="w-4 h-4 mr-2" /> Contact
               </Link>
