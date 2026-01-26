c2a1021f80ba5d0acb04d95fce6043ca35ba896a
Fine-Jason <>
2026-01-26 15:43:40 -0500

    changed contact page card

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -20,9 +20,7 @@ export default function ContactPage() {
         <div className="flex-1">
           <ContactForm />
         </div>
-        <div className="flex-1 flex justify-center ">
-          <GithubRedirectCard />
-        </div>
+        
       </div>
     </PageContainer>
   );
